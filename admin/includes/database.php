b7ff8eb2ff899ee25d9afdf39d7afa0c4089abd7
gibbo101 <lukegibson101@gmail.com>
2022-09-06 20:56:50 +0100

    set database connection

---
+++ admin/includes/database.php
@@ -0,0 +1,23 @@
+<?php
+
+require_once("config.php");
+
+class Database
+{
+
+  private $connection;
+
+  public function open_db_connection()
+  {
+    $this->connection = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+    if (mysqli_connect_errno()) 
+    {
+      die("Database connection error" . mysqli_error());
+    }
+  }
+}
+
+
+$database = new Database();
+
+$databse->open_db_connection();
