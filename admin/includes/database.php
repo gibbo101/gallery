eb64c161feb9ba17a95d9e038bd34a3898bd6417
gibbo101 <lukegibson101@gmail.com>
2022-09-06 21:07:32 +0100

    add query class

--- admin/includes/database.php
+++ admin/includes/database.php
@@ -20,6 +20,16 @@ class Database
       die("Database connection error" . mysqli_error());
     }
   }
+
+  public function query($sql)
+  {
+    $result = mysqli_query($this->connection, $sql);
+    if(!$result)
+    {
+      die("Query failed");
+    }
+    return $result;
+  }
 }
 
 
