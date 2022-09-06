c908da56239d149b97695a7cbddf31e7ef9cc50e
gibbo101 <lukegibson101@gmail.com>
2022-09-06 21:29:32 +0100

    make db connection more oop

--- admin/includes/database.php
+++ admin/includes/database.php
@@ -14,16 +14,17 @@ class Database
 
   public function open_db_connection()
   {
-    $this->connection = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
-    if (mysqli_connect_errno()) 
+    $this->connection = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+    if ($this->connection->connect_errno) 
     {
-      die("Database connection error" . mysqli_error());
+      die("Database connection error" . $this->connection->connect_error);
     }
   }
 
   public function query($sql)
   {
-    $result = mysqli_query($this->connection, $sql);
+    $result = $this->connection->query($sql);
+    $this->confirm_query($result);
     return $result;
   }
 
@@ -31,18 +32,22 @@ class Database
   {
     if(!$result)
     {
-      die("Query failed");
+      die("Query failed" . $this->connection->error);
     }
   }
 
   public function escape_string($string)
   {
-    $escaped_string = mysqli_real_escape_string($this->connection, $string);
+    $escaped_string = $this->connection->real_escape_string($string);
     return $escaped_string;
   }
-}
 
+  public function the_insert_id()
+  {
+    return $this->connection->insert_id;
+  }
 
+} // end of Class Database
 
 
 $database = new Database();
