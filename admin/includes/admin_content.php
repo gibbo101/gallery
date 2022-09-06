ece8558c93c973c45d1b116761653772c90878cc
gibbo101 <lukegibson101@gmail.com>
2022-09-06 21:19:09 +0100

    test database class

--- admin/includes/admin_content.php
+++ admin/includes/admin_content.php
@@ -9,10 +9,13 @@
       </h1>
 
       <?php
-      if($database->connection)
-      {
-        echo "True";
-      }
+      $sql = "SELECT * FROM users WHERE id=1";
+      $result = $database->query($sql);
+      $user_found = mysqli_fetch_array($result);
+      
+      echo $user_found['first_name'];
+
+
       ?>
       <ol class="breadcrumb">
         <li>
