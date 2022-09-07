e80a3243414de83347933968dcef7e86c227865b
gibbo101 <lukegibson101@gmail.com>
2022-09-07 16:51:58 +0100

    assign array values to objects

--- admin/includes/admin_content.php
+++ admin/includes/admin_content.php
@@ -9,15 +9,22 @@
       </h1>
 
       <?php
-      $result_set = User::find_all_users();
+      // $result_set = User::find_all_users();
 
-      while($row = mysqli_fetch_array($result_set))
-      {
-        echo $row['username'] . "<br>";
-      }
-
+      // while($row = mysqli_fetch_array($result_set))
+      // {
+      //   echo $row['username'] . "<br>";
+      // }
       $found_user = User::find_user_by_id(2);
-      echo $found_user['username'];
+      $user = new User();
+      $user->id = $found_user['id'];
+      $user->username = $found_user['username'];
+      $user->password = $found_user['password'];
+      $user->first_name = $found_user['first_name'];
+      $user->last_name = $found_user['last_name'];
+
+      
+     echo $user->id;
 
       ?>
       <ol class="breadcrumb">
