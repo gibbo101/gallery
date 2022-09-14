0efcb6f6f6ab19594c4864efb4491a99a1880a1d
gibbo101 <lukegibson101@gmail.com>
2022-09-14 12:19:12 +0100

    use instantiation to find a user

--- admin/includes/admin_content.php
+++ admin/includes/admin_content.php
@@ -9,23 +9,10 @@
       </h1>
 
       <?php
-      // $result_set = User::find_all_users();
+   
 
-      // while($row = mysqli_fetch_array($result_set))
-      // {
-      //   echo $row['username'] . "<br>";
-      // }
-    //   $found_user = User::find_user_by_id(2);
-      
-    //     $user = User::instantiation($found_user);
-      
-    //  echo $user->id;
-
-     $users = User::find_all_users();
-     foreach($users as $user)
-     {
-      echo $user->username . "<br>";
-     }
+    $found_user = User::find_user_by_id(2);
+    echo $found_user->username;
 
       ?>
       <ol class="breadcrumb">
--- admin/includes/user.php
+++ admin/includes/user.php
@@ -17,10 +17,13 @@ class User
   public static function find_user_by_id($user_id)
   {
     global $database;
-    $result_set = self::find_this_query("SELECT * FROM users WHERE id=$user_id LIMIT 1");
-    $found_user = mysqli_fetch_array($result_set);
+    $the_result_array = self::find_this_query("SELECT * FROM users WHERE id=$user_id LIMIT 1");
+
+    return !empty($the_result_array) ? array_shift($the_result_array) : false;
+
     return $found_user;
   }
+
 
   public static function find_this_query($sql)
   {
