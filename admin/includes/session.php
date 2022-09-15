d50a0f134c6fbbd03338529d451ef5203dd26357
gibbo101 <lukegibson101@gmail.com>
2022-09-15 13:11:48 +0100

    set logout

--- admin/includes/session.php
+++ admin/includes/session.php
@@ -12,18 +12,30 @@ class Session
     $this->check_the_login();
   }
 
+
   public function is_signed_in()
   {
     return $this->signed_in;
   }
+  
 
-  public function login($user) {
+  public function login($user) 
+  {
     if($user)
     {
       $this->user_id = $_SESSION['user_id'] = $user->id;
       $this->signed_in = true;
     }
   }
+
+
+  public function logout()
+  {
+    unset($_SESSION['user_id']);
+    unset($this->user_id);
+    $this->signed_in = false;
+  }
+
 
   private function check_the_login()
   {
